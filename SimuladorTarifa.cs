36aaf87f0c70ea0344d2cc4c42b5bbbe00803710
RicardoC <ricardocubas26@gmail.com>
2026-06-08 23:24:33 -0500

    feat: Implementar calculo base y logica de hora pico, descuento y resumen de vida

--- SimuladorTarifa.cs
+++ SimuladorTarifa.cs
@@ -1,19 +1,25 @@
 public class SimuladorTarifa
 {
-    public static void Maint(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("==========================================");
         Console.WriteLine("    Indrive - Simulador de Tarifa    ");
         Console.WriteLine("==========================================");
         // Datos de entrada.
-        Console.WriteLine("Npombre del pasajero: ");
-        string nombre = Console.ReadLine();
-        
-        Console.WriteLine("Ingrese la distacia del vaaje (Km): ");
-        double distacia = double.Parse(Console.ReadLine());
+        //Declaracion de variables
+        string nombre, nombreVehiculo;
+        double distancia, tarifaBase, costoKm, subtotal, tarifaFinal, descuento;
+        int hora, tipoVehiculo;
+        bool esHoraPico;
+
+        Console.WriteLine("Nombre del pasajero: ");
+        nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese la distacia del viaje (Km): ");
+        distancia = double.Parse(Console.ReadLine());
 
         Console.WriteLine("Hora de salida (0 hrs - 23 hrs): ");
-        int hora = int.Parse(Console.ReadLine());
+        hora = int.Parse(Console.ReadLine());
 
         // \n Es para salto de linea
         Console.WriteLine("\n Tipo de Vehiculo: ");
@@ -21,16 +27,73 @@ public class SimuladorTarifa
         Console.WriteLine(" 2. Confort");
         Console.WriteLine(" 3. Premium");
         Console.WriteLine(" 4. Moto");
-        Console.WriteLine("Ingresa la opcion: ");
-        int tipoVehiculo = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese la opcion: ");
+        tipoVehiculo = int.Parse(Console.ReadLine());
 
         // Proceso
+        switch (tipoVehiculo)
+        {
+            case 1: //Economico
+                nombreVehiculo = "Econimico";
+                tarifaBase = 2.0;
+                costoKm = 1.5;
+                break;
 
+            case 2: //Confort
+                nombreVehiculo = "Confort";
+                tarifaBase = 3.0;
+                costoKm = 2.0;
+                break;
 
+            case 3: // Premium
+                nombreVehiculo = "Premium";
+                tarifaBase = 5.0;
+                costoKm = 3.0;
+                break;
 
+            case 4: // Moto
+                nombreVehiculo = "Moto";
+                tarifaBase = 1.5;
+                costoKm = 1.0;
+                break;
 
-        // Salida
-        
-    
+            default:
+                Console.WriteLine("\n Opcion no valiida. Fin del programa");
+                return;
+
+        }
+        // Cálculo del Subtotal Base
+        subtotal = tarifaBase + (costoKm * distancia);
+        // Condicional de recargo por hora pico
+        if((hora >= 7 && hora <= 9) || (hora >= 17 && hora <= 20))
+        {
+            esHoraPico = true;
+            subtotal = subtotal * 1.30; // incremento del 30%
+        }
+        // Descuento por distancia larga (5%) 
+        if(distancia > 15)
+        {
+            descuento = subtotal * 0.05;
+            subtotal = subtotal - descuento;
+        }
+        // Tarifa minima y redondeo
+        tarifaFinal = Math.Max(subtotal, 5.00);
+        tarifaFinal = Math.Round(tarifaFinal, 2);
+        // Salida - Resumen del Viaje
+        Console.WriteLine("\n===============================================");
+        Console.WriteLine("         RESUMEN DEL VIAJE");
+        Console.WriteLine("===============================================");
+        Console.WriteLine("Pasajero: " + nombre);
+        Console.WriteLine("Vehículo: " + nombreVehiculo);
+        Console.WriteLine("Distancia: " + distancia + " km");
+        Console.WriteLine("Hora Pico: " + (esHoraPico?"Sí (+30%)":"No")); // inline if -> condicional?es verdad: es falso; 
+        if(descuento > 0){
+            Console.WriteLine("Descuento: S/ " + Math.Round(descuento, 2));
+        }else{
+            Console.WriteLine("Descuento: No Aplica");
+        }
+        Console.WriteLine("------------------------------------------");
+        Console.WriteLine("TARIFA FINAL: S/ " + tarifaFinal);
+        Console.WriteLine("===============================================");
     }
 }
